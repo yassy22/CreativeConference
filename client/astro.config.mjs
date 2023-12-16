0702ad26bf402a0f9c740681255573aea8ea62d5
yassy22 <99503819+yassy22@users.noreply.github.com>
2023-12-16 16:24:27 +0100

    Fix broken links in Astro components 🫠🫠

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 // export default defineConfig({});
-// export default defineConfig({
-//   site: "https://devineyasmineyahyeaden.be",
-//   base: "/dist",
-//   image: {
-//     domains: ["localhost"],
-//   },
-// });
+export default defineConfig({
+  site: "https://devineyasmineyahyeaden.be",
+  base: "/dist",
+  image: {
+    domains: ["localhost"],
+  },
+});
